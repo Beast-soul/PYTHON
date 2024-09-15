eb1768a1923a5eae16020d58ebe7db38aa85df79
SOUL OFFICIAL <official.lucky2004@gmail.com>
2024-09-15 13:32:42 +0530

    Add files via upload

---
+++ python_program/main.py
@@ -0,0 +1,10 @@
+print ("Hello Shiv")
+a = 20
+
+b = 60
+
+c = 40
+
+name = "shiv"
+
+print (name)
---
+++ python_program/type.py
@@ -0,0 +1,8 @@
+a = "Baban"
+t = type(a)
+print (t)
+
+a = "25.6"
+b = float(a)
+t = type(b)
+print (t)
